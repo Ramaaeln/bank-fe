524339ee47d3dea0d8c5564b4045a2264b493951
Ramaaeln <abdullahramadanelansary@gmail.com>
2026-01-07 11:39:12 +0700

    first

---
+++ server.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import app from './src/app.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import userRoutes from './routes/user.routes.js';
+
+const app = express();
+
+app.use(cors({
+  origin: ['https://bank-sampah-rust.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API ONLY
+app.use('/api', userRoutes);
+
+// 404 API
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Endpoint tidak ditemukan'
+  });
+});
+
+export default app;
